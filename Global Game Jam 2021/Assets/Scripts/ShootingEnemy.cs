668d34a73669473904ba3035777e4a1ebf8046bc
ThomasApplewhite <44822750+ThomasApplewhite@users.noreply.github.com>
2021-01-30 08:51:48 -0800

    Enemy1 Movement
    It's kinda garbage tier, but the shooting enemies now move on their own in a way I (vaguely) understand. Won't lie, some stuff is happening that I didn't anticipate, but that's okay!

--- Global Game Jam 2021/Assets/Scripts/ShootingEnemy.cs
+++ Global Game Jam 2021/Assets/Scripts/ShootingEnemy.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class ShootingEnemy : MonoBehaviour
 {
+    public float speed = 3f;
+
     BasicPlayerController controller;
     GameObject player;
     Gun gun;
+    Vector2 moveDirection;
+    Rigidbody body;
 
     // Start is called before the first frame update
     void Start()
@@ -14,14 +18,39 @@ public class ShootingEnemy : MonoBehaviour
         controller = this.gameObject.GetComponent<BasicPlayerController>();
         player = GameObject.FindWithTag("Player");
         gun = this.gameObject.GetComponent<Gun>();
+        body = this.gameObject.GetComponent<Rigidbody>();
+
+        moveDirection = Vector2.zero;
 
         //StartCoroutine(Attack());
+        ChangeMovement();
     }
 
     // Update is called once per frame
     void Update()
     {
         gun.Shoot();
+        body.AddRelativeForce(new Vector3(moveDirection.x * speed, 0, moveDirection.y * speed), ForceMode.Impulse);
+    }
+
+    void ChangeMovement()
+    {
+        var pPos = player.transform.position;
+        var oPos = this.gameObject.transform.position;
+
+        //for now let's just pick a random direction
+        moveDirection = new Vector2(
+            Random.Range(0.0f, 1.0f),
+            Random.Range(0.0f, 1.0f)
+        );
+
+        StartCoroutine(movementTime(1.5f));
+    }
+
+    IEnumerator movementTime(float moveTime)
+    {
+        yield return new WaitForSeconds(moveTime);
+        ChangeMovement();
     }
 
     public void DoActorDeath()
@@ -29,3 +58,5 @@ public class ShootingEnemy : MonoBehaviour
         Destroy(this.gameObject);
     }
 }
+
+//playerBody.AddRelativeForce(new Vector3(moveInputs.x * movementSpeed * Time.deltaTime, 0, moveInputs.y * movementSpeed * Time.deltaTime), ForceMode.Impulse);
